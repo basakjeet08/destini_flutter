d31c5ddf218a3ff3b5a1a3e34b8f538988c5e80e
Anirban Basak <jeetbasak2002@gmail.com>
2023-01-04 12:19:31 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Destini());
+
+class Destini extends StatelessWidget {
+  const Destini({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
